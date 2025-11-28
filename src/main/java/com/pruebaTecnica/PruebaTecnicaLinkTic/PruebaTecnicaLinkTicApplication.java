91066d4882ef892f3601b23e8a4f25fbbd1221f8
salamallecum <luisalejandroamayatorres@gmail.com>
2025-11-28 09:45:26 -0500

    AAM - Inicio de proyecto

---
+++ src/main/java/com/pruebaTecnica/PruebaTecnicaLinkTic/PruebaTecnicaLinkTicApplication.java
@@ -0,0 +1,13 @@
+package com.pruebaTecnica.PruebaTecnicaLinkTic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaLinkTicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaLinkTicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pruebaTecnica/PruebaTecnicaLinkTic/PruebaTecnicaLinkTicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pruebaTecnica.PruebaTecnicaLinkTic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaLinkTicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
