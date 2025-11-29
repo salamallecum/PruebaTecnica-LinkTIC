a581f79e32669221143dece9f99cfd071023b2d4
salamallecum <luisalejandroamayatorres@gmail.com>
2025-11-29 17:46:44 -0500

    AAM - Ajuste para el consumo de api desde documentacion

--- src/main/java/com/pruebaTecnica/PruebaTecnicaLinkTic/configuraciones/DocumentacionConfig.java
+++ src/main/java/com/pruebaTecnica/PruebaTecnicaLinkTic/configuraciones/DocumentacionConfig.java
@@ -9,6 +9,10 @@ import io.swagger.v3.oas.annotations.info.License;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 //Clase encargada de la configuracion e información principal de la aplicación que tendrá la documentación Swagger
 @OpenAPIDefinition(
@@ -19,19 +23,16 @@ import io.swagger.v3.oas.annotations.servers.Server;
                 version = "1.0.0",
                 contact = @Contact(name = "Alejo Amaya", url = "https://miportafolioweb.free.nf", email = "luisalejandroamayatorres@gmail.com")
         ),
-        servers = {
-                @Server(description = "DEV SERVER", url = "http://localhost:8080")
-        },
         security = @SecurityRequirement(name = "Security token") //Aqui definimos el esquema de seguridad que tendra la documentacion
 )
-/*//Define el esquema de seguridad de la documentacion
-@SecurityScheme(
-        name = "Security token",
-        description = "Token de acceso de la API",
-        type = SecuritySchemeType.HTTP, //Indica que estamos trabajando con tokens
-        in = SecuritySchemeIn.HEADER,
-        scheme = "Bearer",
-        bearerFormat = "JWT"
-)*/
-public class DocumentacionConfig {}
+
+public class DocumentacionConfig {
+
+    //Bean encargado de corregir la url de acceso a la documentación
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .servers(List.of(new io.swagger.v3.oas.models.servers.Server().url("/pruebatecnica/api/v1")));
+    }
+}
 
